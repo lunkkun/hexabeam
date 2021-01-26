91c5e4c047115243137cae927f8a76ccb146317b
Luuk Linders <lunkkun@gmail.com>
2021-01-26 13:33:16 +0100

    added rim lighting and added select/rotate interactions

--- scenes/Tile.gd
+++ scenes/Tile.gd
@@ -2,15 +2,21 @@ tool
 extends StaticBody
 
 
-const SELECT_ELEVATION = 0.5
+const SELECT_ELEVATION_Z = 0.35
+const SELECT_ELEVATION_TIME = 0.25
+const ROTATE_LEFT = 60
+const ROTATE_RIGHT = -60
+const ROTATE_TIME = 0.2
 
 export(int, 1, 2) var layout = 1 setget set_layout
-export(int, 0, 5) var orientation setget set_orientation
+export(int, 0, 5) var orientation setget set_orientation # initial
 
 var active = false setget set_active
 var selected = false setget set_selected
 var buffered_selected = false
 var can_rotate = false
+
+var _rotating = false
 
 onready var initial_z = translation.z
 onready var _tubes = $Tubes.get_children()
@@ -21,6 +27,12 @@ func _ready():
 		tube.connect("connected", self, "_on_Tube_connected", [tube])
 		tube.connect("disconnected", self, "_on_Tube_disconnected", [tube])
 
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("rotate_left"):
+		tween_rotate(ROTATE_LEFT)
+	elif event.is_action_pressed("rotate_right"):
+		tween_rotate(ROTATE_RIGHT)
 
 func _input_event(_camera, event, _click_position, _click_normal, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
@@ -54,20 +66,32 @@ func set_selected(value: bool):
 		return
 	
 	buffered_selected = value
-	can_rotate = false
 	
 	if $Tween.is_active():
 		yield($Tween, "tween_completed")
 	
 	selected = value
+	can_rotate = selected
 	
-	var to = initial_z + selected * SELECT_ELEVATION
-	$Tween.interpolate_property(self, "translation:z", null, to, 0.4)
+	var to = initial_z + (SELECT_ELEVATION_Z if selected else 0)
+	$Tween.interpolate_property(self, "translation:z", null, to, SELECT_ELEVATION_TIME)
+	$Tween.start()
+
+
+func tween_rotate(degrees):
+	if not can_rotate:
+		return
+	
+	can_rotate = false # prevent further rotation inputs
+	
+	if $Tween.is_active():
+		yield($Tween, "tween_completed")
+	
+	var to = rotation_degrees.z + degrees
+	$Tween.interpolate_property(self, "rotation_degrees:z", null, to, ROTATE_TIME)
 	$Tween.start()
 	
-	yield($Tween, "tween_completed")
-	
-	can_rotate = selected
+	can_rotate = selected # accept rotation inputs again
 
 
 func _on_Tube_connected(_tube):
