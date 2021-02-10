bf5254e4c12d4c8310c7a7cda8dc2f94d462cbe0
Luuk Linders <lunkkun@gmail.com>
2021-02-10 14:36:40 +0100

    some refacturing and make sure tiles are not selectable when transitioning out

--- scenes/Level.gd
+++ scenes/Level.gd
@@ -19,34 +19,40 @@ func _ready():
 
 
 func transition_in(transition_time, transition_x):
+	_transition_x(transition_time, transition_x)
+	_transition_tiles(transition_time)
+
+
+func transition_out(transition_time, transition_x):
+	_transition_x(transition_time, transition_x)
+
+
+func _transition_x(transition_time, transition_x):
 	for tile in _tiles:
 		tile.selected = false
 		tile.input_ray_pickable = false
+		
+	var to_x = translation.x + transition_x
+	$TransitionTween.interpolate_property(self, "translation:x", null, to_x, transition_time)
+	$TransitionTween.start()
+
+
+func _transition_tiles(transition_time):
+	for tile in _tiles:
 		tile.translation.z -= TRANSITION_Z
 		tile.rotation_degrees.z -= TRANSITION_DEG_Z
 	
 	var tile_time_offset = transition_time / 2 / _tile_count
 	tile_transition_time = transition_time - (tile_time_offset * (_tile_count - 1))
-	print("transition_time: " + str(transition_time))
 	
 	tiles_transitioned = 0
-	transition_tile()
+	_transition_tile()
 	
 	$TransitionTimer.wait_time = tile_time_offset
 	$TransitionTimer.start()
-	
-	var to_x = translation.x + transition_x
-	$TransitionTween.interpolate_property(self, "translation:x", null, to_x, transition_time)
-	$TransitionTween.start()
 
 
-func transition_out(transition_time, transition_x):
-	var to_x = translation.x + transition_x
-	$TransitionTween.interpolate_property(self, "translation:x", null, to_x, transition_time)
-	$TransitionTween.start()
-
-
-func transition_tile():
+func _transition_tile():
 	if tiles_transitioned < _tile_count:
 		print("transitioning tile " + str(tiles_transitioned))
 		var tile = _tiles[tiles_transitioned]
@@ -55,7 +61,7 @@ func transition_tile():
 
 
 func _on_TransitionTimer_timeout():
-	transition_tile()
+	_transition_tile()
 	
 	if tiles_transitioned >= _tile_count:
 		$TransitionTimer.stop()
